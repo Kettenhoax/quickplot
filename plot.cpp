3ba8029585384ecb639bfeba939eb4ee610f2719
Marcel Zeilinger <marcel.zeilinger@ait.ac.at>
2021-06-01 11:59:17 +0200

    Set negative time axes ticks
    Try implement docking

--- plot.cpp
+++ plot.cpp
@@ -1,6 +1,4 @@
 #include <Mahi/Gui.hpp>
-#include <Mahi/Util.hpp>
-
 #include <mutex>
 #include <deque>
 #include <rclcpp/rclcpp.hpp>
@@ -63,13 +61,38 @@ private:
   std::vector<double> times_;
   std::vector<double> data_;
 
+  std::vector<double> history_tick_values_;
+  std::vector<std::string> history_tick_labels_;
+
 public:
   QuickPlot(std::shared_ptr<BufferNode> _node)
-  : Application(640, 480, "QuickPlot"), node_(_node) {}
+  : Application(640, 480, "QuickPlot"), node_(_node) {
+    set_vsync(false);
+    ImGui::DisableViewports();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+  }
 
   void update() override
   {
-    ImGui::Begin("QuickPlot");
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    auto vec2 = get_window_size();
+    ImGui::SetNextWindowPos({0,0}, ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowSize(ImVec2(vec2.x, vec2.y), ImGuiCond_FirstUseEver);
+    ImGui::SetNextWindowBgAlpha(0.0f);
+
+    if (!ImGui::Begin("QuickPlot", nullptr, ImGuiWindowFlags_NoTitleBar)) {
+      // Early out if the window is collapsed, as an optimization.
+      ImGui::End();
+      return;
+    }
+    ImGui::PopStyleVar();
+
+    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_PassthruCentralNode | ImGuiWindowFlags_NoBackground;
+    ImGuiID dockspace_id = ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+
+    ImGui::SetNextWindowDockID(dockspace_id, ImGuiCond_FirstUseEver);
+
     node_->take(times_, data_);
 
     static float history_sec = 10.0f;
@@ -79,16 +102,31 @@ public:
     auto end_sec = t.seconds();
     auto start_sec = (t - history_dur).seconds();
 
-    static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
     ImPlot::SetNextPlotLimitsX(start_sec, end_sec, ImGuiCond_Always);
     ImPlot::SetNextPlotLimitsY(-2, 2);
+
+    size_t n_ticks = history_sec;
+    if (n_ticks != history_tick_values_.size()) {
+      history_tick_labels_.clear();
+      history_tick_values_.clear();
+      for (int i = n_ticks; i >= 0; i--) {
+        history_tick_labels_.push_back(std::to_string(-i));
+        history_tick_values_.push_back(end_sec - i);
+      }
+    }
+
+    std::vector<char const *> tick_cstrings;
+    tick_cstrings.reserve(history_tick_labels_.size());
+    for (const auto & labels : history_tick_labels_) {
+      tick_cstrings.push_back(const_cast<char *>(labels.c_str()));
+    }
+    ImPlot::SetNextPlotTicksX(
+      history_tick_values_.data(),
+      history_tick_values_.size(), tick_cstrings.data());
+
     if (ImPlot::BeginPlot(
-        "speed", "t", "m/s", ImGui::ImVec2(-1, -1), ImPlot::ImPlotFlags_None,
-        ImPlot::ImPlotAxisFlags_Lock))
+        "speed", "t (header.stamp sec)", "m/s", ImVec2(-1, -1), ImPlotFlags_None))
     {
-      int n_ticks = history_sec;
-      std::vector<char*> 
-      ImPlot::SetNextPlotTicksX()
       ImPlot::PlotLine(
         "cmd_4ws", times_.data(), data_.data(), times_.size());
       ImPlot::EndPlot();
