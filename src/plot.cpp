603679e092d9a3d4d199fc8a857fe06a324d8ba9
Marcel Zeilinger <marcel.zeilinger@ait.ac.at>
2021-07-07 07:48:43 +0200

    Non-crashing generic subscriber

--- src/plot.cpp
+++ src/plot.cpp
@@ -6,23 +6,109 @@
 #include <utility>
 #include <memory>
 #include <vector>
+#include <unordered_map>
 #include <rclcpp/rclcpp.hpp>
-#include <rosidl_typesupport_introspection_cpp/field_types.hpp>
-#include <rosidl_typesupport_introspection_cpp/message_introspection.hpp>
 #include <rosidl_typesupport_cpp/identifier.hpp>
 #include <rosidl_typesupport_introspection_cpp/identifier.hpp>
 #include "quickplot/typesupport_helpers.hpp"
+#include "quickplot/parser.hpp"
+#include <boost/circular_buffer.hpp>
 
 using mahi::gui::Application;
 using std::placeholders::_1;
+using std::vector;
+using std::string;
 
 namespace quickplot
 {
 
-struct TopicData
+struct MessageData
 {
   rclcpp::Time t;
-  double value;
+  std::vector<double> value;
+};
+
+class TopicBuffer
+{
+private:
+  std::string topic_type_;
+  std::shared_ptr<rcpputils::SharedLibrary> type_support_library_;
+  std::shared_ptr<rcpputils::SharedLibrary> introspection_support_library_;
+  const rosidl_message_type_support_t * type_support_handle_;
+  const rosidl_message_type_support_t * introspection_support_handle_;
+
+  uint8_t* deserialized_message_buffer_;
+  MessageDataBuffer data_buffer_;
+
+public:
+  rclcpp::GenericSubscription::SharedPtr subscription;
+  std::vector<MessageData> received_data_;
+
+  explicit TopicBuffer(std::string topic_type)
+  : topic_type_(topic_type)
+  {
+    type_support_library_ = quickplot::get_typesupport_library(
+      topic_type,
+      rosidl_typesupport_cpp::typesupport_identifier);
+    type_support_handle_ = quickplot::get_typesupport_handle(
+      topic_type,
+      rosidl_typesupport_cpp::typesupport_identifier,
+      type_support_library_);
+
+    introspection_support_library_ = quickplot::get_typesupport_library(
+      topic_type_,
+      rosidl_typesupport_introspection_cpp::typesupport_identifier);
+    introspection_support_handle_ = quickplot::get_typesupport_handle(
+      topic_type_,
+      rosidl_typesupport_introspection_cpp::typesupport_identifier,
+      introspection_support_library_);
+
+    auto members =
+      static_cast<const rosidl_typesupport_introspection_cpp::MessageMembers *>(
+      introspection_support_handle_
+      ->data);
+    deserialized_message_buffer_ = new uint8_t[members->size_of_];
+    members->init_function(
+      deserialized_message_buffer_,
+      rosidl_runtime_cpp::MessageInitialization::ALL);
+  }
+
+  ~TopicBuffer()
+  {
+    auto members =
+      static_cast<const rosidl_typesupport_introspection_cpp::MessageMembers *>(
+      introspection_support_handle_
+      ->data);
+    members->fini_function(deserialized_message_buffer_);
+    delete[] deserialized_message_buffer_;
+  }
+
+  void add_field(std::vector<std::string> member_path)
+  {
+    data_buffer_.data.push_back(
+      {
+        member_path,
+        0.0
+      });
+  }
+
+  void receive(std::shared_ptr<rclcpp::SerializedMessage> message)
+  {
+    // TODO(ZeilingerM) not synchronized with container
+    quickplot::MessageDataBuffer buffer;
+    auto & linear_x_data = buffer.data.emplace_back();
+    linear_x_data.member_path = {"twist", "linear", "x"};
+
+    quickplot::parse_generic_message(
+      type_support_handle_, introspection_support_handle_,
+      *message, deserialized_message_buffer_, data_buffer_);
+
+    received_data_.push_back(
+      {
+        data_buffer_.stamp,
+        {data_buffer_.data[0].value},
+      });
+  }
 };
 
 class BufferNode : public rclcpp::Node
@@ -33,33 +119,55 @@ public:
   {
   }
 
-  std::mutex data_mutex_;
-  std::deque<TopicData> data_queue_;
+  mutable std::mutex topic_mutex_;
+  mutable std::mutex data_mutex_;
+  std::unordered_map<std::string, TopicBuffer> topic_buffers_;
 
-  void take(std::vector<double> & times, std::vector<double> & values)
+  void add_topic_field(
+    std::string topic, std::string topic_type,
+    std::vector<std::string> member_path)
   {
-    std::unique_lock<std::mutex> lock(data_mutex_);
-    while (!data_queue_.empty()) {
-      auto data = data_queue_.front();
-      times.push_back(data.t.seconds());
-      values.push_back(data.value);
-      data_queue_.pop_front();
+    std::unique_lock<std::mutex> lock(topic_mutex_);
+    auto it = topic_buffers_.try_emplace(topic, topic_type);
+    auto & buffer = it.first->second;
+    buffer.add_field(member_path);
+    if (it.second) {
+      // only create subscription if entry was added first
+      buffer.subscription = this->create_generic_subscription(
+        topic, topic_type, rclcpp::SensorDataQoS(),
+        std::bind(&TopicBuffer::receive, &buffer, _1));
     }
   }
 
-private:
-  // void data_callback(const FourWheelSteeringStamped::SharedPtr msg)
-  // {
-  //   TopicData data;
-  //   data.t = rclcpp::Time(msg->header.stamp);
-  //   data.value = msg->data.speed;
-  //   {
-  //     std::unique_lock<std::mutex> lock(data_mutex_);
-  //     data_queue_.push_back(data);
-  //   }
-  // }
+  std::vector<std::string> active_topics()
+  {
+    std::unique_lock<std::mutex> lock(topic_mutex_);
+    std::vector<std::string> topics;
+    topics.reserve(topic_buffers_.size());
+    for (auto kv : topic_buffers_) {
+      topics.push_back(kv.first);
+    }
+    return topics;
+  }
 
-  // rclcpp::Subscription<FourWheelSteeringStamped>::SharedPtr subscription_;
+  std::vector<MessageData> take(std::string topic)
+  {
+    std::unique_lock<std::mutex> lock(data_mutex_);
+    auto it = topic_buffers_.find(topic);
+    if (it == topic_buffers_.end()) {
+      throw std::invalid_argument("Topic " + topic + " not active");
+    }
+    auto [_, topic_buffer] = *it;
+    auto copy = std::vector(topic_buffer.received_data_);
+    topic_buffer.received_data_.clear();
+    return copy;
+  }
+};
+
+struct PlotData
+{
+  std::vector<double> times;
+  std::vector<std::vector<double>> values;
 };
 
 class QuickPlot : public Application
@@ -68,14 +176,11 @@ private:
   std::shared_ptr<BufferNode> node_;
   rclcpp::Event::SharedPtr graph_event_;
 
-  std::vector<double> times_;
-  std::vector<double> data_;
-
   std::vector<double> history_tick_values_;
   std::vector<std::string> history_tick_labels_;
 
-  std::vector<std::pair<std::string, std::string>> topics_;
-  std::vector<std::string> active_topics_;
+  std::unordered_map<std::string, std::string> available_topics_to_types_;
+  std::unordered_map<std::string, PlotData> active_topics_to_data_;
 
 public:
   explicit QuickPlot(std::shared_ptr<BufferNode> _node)
@@ -87,44 +192,8 @@ public:
     graph_event_ = node_->get_graph_event();
   }
 
-  void add_topic(std::string topic_name, std::string topic_type)
-  {
-    active_topics_.push_back(topic_name);
-
-    auto introspection_library = get_typesupport_library(
-      topic_type,
-      rosidl_typesupport_introspection_cpp::typesupport_identifier);
-    auto introspection_support = get_typesupport_handle(
-      topic_type,
-      rosidl_typesupport_introspection_cpp::typesupport_identifier,
-      introspection_library);
-
-    // auto support_library = get_typesupport_library(
-    //   topic_type,
-    //   rosidl_typesupport_cpp::typesupport_identifier);
-    // auto type_support = get_typesupport_handle(
-    //   topic_type,
-    //   rosidl_typesupport_cpp::typesupport_identifier,
-    //   support_library);
-      
-    // const auto* members = static_cast<const rosidl_typesupport_introspection_cpp::MessageMembers*>(introspection_support->data);
-    // if (message_type_has_header(introspection_support)) {
-
-    // }
-
-    auto subscription = node_->create_generic_subscription(
-      topic_name,
-      topic_type,
-      rclcpp::SensorDataQoS(),
-      [this, topic_name](std::shared_ptr<rclcpp::SerializedMessage> message) {
-        auto serialized_message = message->get_rcl_serialized_message();
-      });
-  }
-
   void update() override
   {
-    node_->take(times_, data_);
-
     ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
     auto vec2 = get_window_size();
     ImGui::SetNextWindowPos({0, 0}, ImGuiCond_FirstUseEver);
@@ -144,23 +213,30 @@ public:
     ImGui::SetNextWindowDockID(dockspace_id, ImGuiCond_FirstUseEver);
 
     if (graph_event_->check_and_clear()) {
-      topics_.clear();
       auto topics_and_types = node_->get_topic_names_and_types();
-      for (const auto & pair : topics_and_types) {
-        if (pair.second.size() != 1) {
-          std::cerr << "Topic " << pair.first << " has multiple types, quickplot will ignore it" <<
+      for (const auto & [topic, types] : topics_and_types) {
+        if (types.size() != 1) {
+          std::cerr << "Topic " << topic << " has multiple types, quickplot will ignore it" <<
             std::endl;
           continue;
         }
-        topics_.push_back(std::make_pair(pair.first, pair.second[0]));
+        auto new_type = types[0];
+        auto [entry, inserted] = available_topics_to_types_.try_emplace(topic, new_type);
+        if (!inserted) {
+          // the topic was already in the map, so we possibly have active listeners
+          // parsing a message type that may have changed
+          if (entry->second != new_type) {
+            std::invalid_argument("Type of topic " + topic + " changed");
+          }
+        }
       }
     }
 
-    for (const auto & topic : topics_) {
+    for (const auto & topic : available_topics_to_types_) {
       if (ImGui::BeginDragDropSource(ImGuiDragDropFlags_None)) {
         ImGui::SetDragDropPayload("topic_name", topic.first.c_str(), topic.first.size());
         // ImPlot::ItemIcon(dnd[k].Color); ImGui::SameLine();
-        ImGui::Text(topic.first.c_str());
+        ImGui::Text("%s", topic.first.c_str());
         ImGui::EndDragDropSource();
       }
     }
@@ -201,13 +277,36 @@ public:
         if (ImPlot::BeginDragDropTargetY(y)) {
           if (const ImGuiPayload * payload = ImGui::AcceptDragDropPayload("topic_name")) {
             auto topic_name = std::string(static_cast<char *>(payload->Data));
+            node_->add_topic_field(
+              topic_name, available_topics_to_types_[topic_name], {"data",
+                "speed"});
           }
           ImPlot::EndDragDropTarget();
         }
       }
-      for (const auto & topic : active_topics_) {
-        ImPlot::PlotLine(
-          topic.c_str(), times_.data(), data_.data(), times_.size());
+      for (const auto & topic : node_->active_topics()) {
+        auto new_data = node_->take(topic);
+        if (new_data.empty()) {
+          continue;
+        }
+        
+        auto [entry, _] = active_topics_to_data_.try_emplace(topic);
+        auto [plot_topic, plot_data] = *entry;
+
+        plot_data.values.resize(new_data[0].value.size());
+
+        for (const auto & item : new_data) {
+          plot_data.times.push_back(item.t.seconds());
+          for (size_t i = 0; i < item.value.size(); i++) {
+            plot_data.values[i].push_back(item.value[i]);
+          }
+        }
+
+        for (size_t i = 0; i < plot_data.values.size(); i++) {
+          ImPlot::PlotLine(
+            topic.c_str(), plot_data.times.data(), plot_data.values[i].data(),
+            plot_data.times.size());
+        }
       }
       ImPlot::EndPlot();
     }
